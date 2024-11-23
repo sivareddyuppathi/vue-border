60af42bc2da7045ec38ccdc3d70fde5154ef16a0
Reddy Uppathi <popvictoria@gmail.com>
2024-11-24 02:14:33 +0530

    update: remove unused types

--- src/components/tpyes.d.ts
+++ src/components/tpyes.d.ts
@@ -1,18 +1,3 @@
-/**
- * Default CSS definition for typescript,
- * will be overridden with file-specific definitions by rollup
- */
-// declare module '*.css' {
-//   const content: { [className: string]: string };
-//   export default content;
-// }
-
-// declare module '*.svg' {
-//   const svgUrl: string;
-//   const svgComponent: SvgrComponent;
-//   export default svgUrl;
-//   export { svgComponent as Component }
-// }
 
 export interface Props {
   style?: Record<string, any>;
@@ -21,25 +6,14 @@ export interface Props {
   borderRadius?: number | string;
   borderColor?: string;
   borderType?: string;
-
-  topElement?: any;
   topPosition?: number;
   topOffset?: number | string;
-
-  rightElement?: any;
   rightPosition?: number;
   rightOffset?: number | string;
-  rightGap?: number | string;
-
-  bottomElement?: any;
   bottomPosition?: number;
   bottomOffset?: number | string;
-  bottomGap?: number | string;
-
-  leftElement?: any;
   leftPosition?: number;
   leftOffset?: number | string;
-  leftGap?: number | string;
 }
 
 export type Positions = {
