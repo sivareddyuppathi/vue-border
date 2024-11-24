3c3e7328c43262f85eaaeee843a0d5fdb5615cb3
Reddy Uppathi <popvictoria@gmail.com>
2024-11-24 20:05:54 +0530

    add: add base to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,20 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+// import { ghPages } from 'vite-plugin-gh-pages';
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    // ghPages()
   ],
+  base: '/vue-border/',
+  build: {
+    outDir: 'dist', // Ensure this is the correct directory
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
